01ffb9e5fdc50cf9112ae2706357636f699d399f
Bob Dröge <b.e.droge@rug.nl>
2021-07-08 14:34:04 +0200

    use variables for filenames

--- test/test_jobinfo.py
+++ test/test_jobinfo.py
@@ -6,7 +6,13 @@ import os
 import pytest
 import re
 
+# Directory that contains the input files for the tests.
 DATA_DIR = 'data'
+# Filenames of the input files.
+SACCT_FILE = 'sacct.txt'
+SSTAT_FILE = 'sstat.txt'
+SQUEUE_FILE = 'squeue.txt'
+SCONTROL_FILE = 'scontrol.txt'
 
 # This is not a proper module, so use importlib to load the sources.
 pynumparser_loader = importlib.machinery.SourceFileLoader('pynumparser', '../pynumparser.py')
@@ -22,7 +28,7 @@ Subprocess = namedtuple('Subprocess', ['stdout'])
 def sacct_output(jobid):
     '''Mock call to sacct by reading from a file.'''
 
-    with open(os.path.join(DATA_DIR, 'sacct.txt'), 'rb') as sacct_file:
+    with open(os.path.join(DATA_DIR, SACCT_FILE), 'rb') as sacct_file:
         sacct_lines = sacct_file.readlines()
     jobid_lines = [line for line in sacct_lines if line.split(b'\xe2\x98\x83')[0].split(b'.')[0] == jobid]
     return Subprocess(stdout = jobid_lines)
@@ -30,7 +36,7 @@ def sacct_output(jobid):
 
 def squeue_output(jobid):
     '''Mock call to squeue by reading from a file.'''
-    with open(os.path.join(DATA_DIR, 'squeue.txt'), 'r') as squeue_file:
+    with open(os.path.join(DATA_DIR, SQUEUE_FILE), 'r') as squeue_file:
         squeue_lines = squeue_file.read()
 
     squeue_line = re.search(f'{jobid}.*\n', squeue_lines).group(0).strip().split('|', 1)[1]
@@ -40,7 +46,7 @@ def squeue_output(jobid):
 
 def sstat_output(jobid):
     '''Mock call to sstat by reading from a file.'''
-    with open(os.path.join(DATA_DIR, 'sstat.txt'), 'rb') as sstat_file:
+    with open(os.path.join(DATA_DIR, SSTAT_FILE), 'rb') as sstat_file:
         sstat_lines = sstat_file.readlines()
 
     jobid_lines = [
@@ -54,7 +60,7 @@ def sstat_output(jobid):
 
 def scontrol_show_nodes_output(node):
     '''Mock call to scontrol by reading from a file.'''
-    with open(os.path.join(DATA_DIR, 'nodes.txt'), 'r') as nodes_file:
+    with open(os.path.join(DATA_DIR, SCONTROL_FILE), 'r') as nodes_file:
         nodes_lines = nodes_file.read()
     node_line = re.search(f'NodeName={node}.*\n', nodes_lines).group(0).strip().encode('UTF-8')
     return Subprocess(stdout = io.BytesIO(node_line))
@@ -79,7 +85,7 @@ def popen_side_effect(*args, **kwargs):
 def find_all_jobids():
     '''Find all our test job ids in the sacct.txt file.'''
     jobids = []
-    with open(os.path.join(DATA_DIR, 'sacct.txt'), 'rb') as sacct_file:
+    with open(os.path.join(DATA_DIR, SACCT_FILE), 'rb') as sacct_file:
         sacct = sacct_file.readlines()
         jobids = [line.split(b'\xe2\x98\x83', 1)[0].split(b'.')[0].decode() for line in sacct]
     # convert to set to remove duplicates
